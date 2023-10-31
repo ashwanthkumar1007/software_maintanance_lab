4d39223e7081305da815344915a1987f24ae2bad
ashwanthkumar1007 <67331617+ashwanthkumar1007@users.noreply.github.com>
2023-10-31 15:00:14 -0700

    Persisting only the calculate area function

--- Duplicate_code_example.py
+++ Duplicate_code_example.py
@@ -1,5 +1,4 @@
 #Duplicate code example
-# Duplicate code: calculate_area and calculate_perimeter functions have similar calculations.
 def calculate_area_perimeter(length, width):
     area = length * width
     perimeter = 2 * (length + width)
